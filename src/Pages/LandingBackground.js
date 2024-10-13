caf80622f26721ae790757727141749e9cf71fed
Anushka <anushkavarshney2708@gmail.com>
2024-10-13 19:39:29 +0530

    Built landing page background

---
+++ src/Pages/LandingBackground.js
@@ -0,0 +1,20 @@
+export default function LandingBackground() {
+  return (
+    <div className="landing-bg-main">
+      <h1 className="landing-bg-heading">welcome to alabay world</h1>
+      <div className="landing-bg-content">
+        <p>
+          where the{" "}
+          <span className="content-span">
+            legendary Central Asian Shepherd Dog{" "}
+          </span>
+          meets a new-age adventure.{" "}
+          <span className="content-span">Join us </span> in celebrating the
+          <span className="content-span"> strength</span>,
+          <span className="content-span"> loyalty</span>, and
+          <span className="content-span"> heritage</span> of the Alabay breed.
+        </p>
+      </div>
+    </div>
+  );
+}
